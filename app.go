f78f55e370e0a43a8f3597d30061669768578a1a
Roman Frołow <rofrol@gmail.com>
2012-07-11 08:53:59 +0200

    func main moved to top

--- app.go
+++ app.go
@@ -29,6 +29,57 @@ import (
 // http://en.wikipedia.org/wiki/HTTP_cookie
 var store = sessions.NewCookieStore([]byte("something-very-secret"))
 
+
+var addr = flag.String("addr", ":9000", "http service address") // Q=17, R=18
+
+func main() {
+	store.Options.Secure = true
+	flag.Parse()
+	// If only one version can be returned (i.e., the other redirects to it), that’s great!
+	// http://googlewebmastercentral.blogspot.com/2010/04/to-slash-or-not-to-slash.html
+	r := mux.NewRouter().StrictSlash(true)
+	// If a site is accessed over HTTPS and loads some parts of a page over insecure HTTP, the user might still be vulnerable to some attacks or some kinds of surveillance. For instance, the New York Times makes many HTML pages available in HTTPS, but other resources such as images, CSS, JavaScript, or third party ads and tracking beacons, are only loadable over HTTP. That means that these resources are sent unencrypted, and someone spying on you could probably infer the article you were reading.
+	// There are also potential vulnerabilities when parts of a page are loaded over HTTP because an attacker might replace them with versions containing false information, or Javascript code that helps the attacker spy on the user or take over an account.
+	//
+	// In order to [enable HTTPS by default for Gmail] we had to deploy
+	// no additional machines and no special hardware. On our production
+	// frontend machines, SSL/TLS accounts for less than 1% of the CPU load, less
+	// than 10KB of memory per connection and less than 2% of network overhead.
+	// www.eff.org/https-everywhere/faq
+
+	// no refferal with https?
+
+	//Not needed, because there is redirecting
+	//s := r.Schemes("https").Subrouter()
+	r.HandleFunc("/", http.HandlerFunc(index))
+	// REST good practics: trailing slash denotes a directory, while the lack of it denotes a file/resource
+	// http://techblog.bozho.net/?p=401
+	r.HandleFunc("/login", http.HandlerFunc(login))
+	r.HandleFunc("/register", http.HandlerFunc(register))
+	r.HandleFunc("/logout", http.HandlerFunc(logout))
+	r.HandleFunc("/products", http.HandlerFunc(products))
+	r.HandleFunc("/products/add", http.HandlerFunc(productsAdd))
+	r.HandleFunc("/users", http.HandlerFunc(users))
+	http.Handle("/", r)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+
+	// starting in goroutines with error reporting, thanks to davecheney from #go-nuts
+	// like this: http://serverfault.com/questions/67316/in-nginx-how-can-i-rewrite-all-http-requests-to-https-while-maintaining-sub-dom
+	// It will direct from http://localhost/users to https://localhost:9999/users, but not from http://localhost:9999/users
+	go func() {
+		log.Fatalf("ListenAndServe: %v", http.ListenAndServe(":8080", http.HandlerFunc(notlsHandler)))
+	}()
+	go func() {
+		log.Fatalf("ListenAndServeTLS: %v", http.ListenAndServeTLS(*addr, "tls/cert.pem", "tls/key.pem", nil))
+	}()
+	select {}
+
+	// Testing redirecting
+	// curl -v http://localhost:8080/products
+	// Or if you made forwarding or run as superuser so you have access to ports below 1024
+	// curl -v http://localhost/products
+}
+
 func register(w http.ResponseWriter, r *http.Request) {
 	if err := r.ParseForm(); err != nil {
 		serveError(w, err)
@@ -428,54 +479,4 @@ func notlsHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("in notlsHandler")
 	fullUrl := "https://localhost" + r.RequestURI
 	http.Redirect(w, r, fullUrl, http.StatusMovedPermanently)
-}
-
-var addr = flag.String("addr", ":9000", "http service address") // Q=17, R=18
-
-func main() {
-	store.Options.Secure = true
-	flag.Parse()
-	// If only one version can be returned (i.e., the other redirects to it), that’s great!
-	// http://googlewebmastercentral.blogspot.com/2010/04/to-slash-or-not-to-slash.html
-	r := mux.NewRouter().StrictSlash(true)
-	// If a site is accessed over HTTPS and loads some parts of a page over insecure HTTP, the user might still be vulnerable to some attacks or some kinds of surveillance. For instance, the New York Times makes many HTML pages available in HTTPS, but other resources such as images, CSS, JavaScript, or third party ads and tracking beacons, are only loadable over HTTP. That means that these resources are sent unencrypted, and someone spying on you could probably infer the article you were reading.
-	// There are also potential vulnerabilities when parts of a page are loaded over HTTP because an attacker might replace them with versions containing false information, or Javascript code that helps the attacker spy on the user or take over an account.
-	//
-	// In order to [enable HTTPS by default for Gmail] we had to deploy
-	// no additional machines and no special hardware. On our production
-	// frontend machines, SSL/TLS accounts for less than 1% of the CPU load, less
-	// than 10KB of memory per connection and less than 2% of network overhead.
-	// www.eff.org/https-everywhere/faq
-
-	// no refferal with https?
-
-	//Not needed, because there is redirecting
-	//s := r.Schemes("https").Subrouter()
-	r.HandleFunc("/", http.HandlerFunc(index))
-	// REST good practics: trailing slash denotes a directory, while the lack of it denotes a file/resource
-	// http://techblog.bozho.net/?p=401
-	r.HandleFunc("/login", http.HandlerFunc(login))
-	r.HandleFunc("/register", http.HandlerFunc(register))
-	r.HandleFunc("/logout", http.HandlerFunc(logout))
-	r.HandleFunc("/products", http.HandlerFunc(products))
-	r.HandleFunc("/products/add", http.HandlerFunc(productsAdd))
-	r.HandleFunc("/users", http.HandlerFunc(users))
-	http.Handle("/", r)
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
-
-	// starting in goroutines with error reporting, thanks to davecheney from #go-nuts
-	// like this: http://serverfault.com/questions/67316/in-nginx-how-can-i-rewrite-all-http-requests-to-https-while-maintaining-sub-dom
-	// It will direct from http://localhost/users to https://localhost:9999/users, but not from http://localhost:9999/users
-	go func() {
-		log.Fatalf("ListenAndServe: %v", http.ListenAndServe(":8080", http.HandlerFunc(notlsHandler)))
-	}()
-	go func() {
-		log.Fatalf("ListenAndServeTLS: %v", http.ListenAndServeTLS(*addr, "tls/cert.pem", "tls/key.pem", nil))
-	}()
-	select {}
-
-	// Testing redirecting
-	// curl -v http://localhost:8080/products
-	// Or if you made forwarding or run as superuser so you have access to ports below 1024
-	// curl -v http://localhost/products
 }
