dabce875df62052677bbe93ac93fe145093a12a8
Roman Frołow <rofrol@gmail.com>
2014-01-30 13:02:20 +0000

    added info about init.sh, modified init.sh so it can be run from anywhere

--- db/init.sh
+++ db/init.sh
@@ -1,1 +1,10 @@
-sqlite3 app.db < schema.sql
+#!/usr/bin/env bash
+
+function log {
+	[ "$DEBUG" == "1" ] && echo "$@"
+}
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cmd="sqlite3 $DIR/app.db < $DIR/schema.sql"
+log "cmd=$cmd"
+eval $cmd
